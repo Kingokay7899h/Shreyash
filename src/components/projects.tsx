eb42ec332a133872bebf2af12c690f7d76bed270
Kingokay7899h <shreyashdesai60@gmail.com>
2025-08-15 15:55:43 +0530

    Update projects.tsx

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,6 +1,5 @@
 import React, { useRef, useState } from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
-import { useGLTF } from '@react-three/drei';
 import { Float, PerspectiveCamera, Environment, Html } from '@react-three/drei';
 import { motion, AnimatePresence } from 'framer-motion';
 import * as THREE from 'three';
